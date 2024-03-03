b1da7626465306a7db1bfe6dd4fb0bec310cd7a8
Humberto Dias <hldtux@gmail.com>
2024-03-03 19:38:45 -0300

    add SDL2_ttf

---
+++ scripts/008_SDL2_ttf-2.22.0.sh
@@ -0,0 +1,15 @@
+VERSION=2.22.0
+wget https://github.com/libsdl-org/SDL_ttf/releases/download/release-$VERSION/SDL2_ttf-$VERSION.tar.gz -O SDL2_ttf.tar.gz
+
+## Unpack the source code.
+rm -Rf SDL2_ttf && mkdir SDL2_ttf && tar --strip-components=1 --directory=SDL2_ttf -xvzf SDL2_ttf.tar.gz && cd SDL2_ttf
+
+./configure --prefix="$PS3DEV/portlibs/ppu" --host=powerpc64-ps3-elf \
+	--with-freetype-exec-prefix="$PS3DEV/portlibs/ppu" \
+	--with-sdl-exec-prefix="$PS3DEV/portlibs/ppu" \
+	--without-x \
+	--disable-sdltest \
+	--disable-harfbuzz \
+    	|| { exit 1; }
+
+make && make install || { exit 1; }
