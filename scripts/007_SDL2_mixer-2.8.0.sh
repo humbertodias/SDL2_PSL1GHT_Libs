46fc8e37cd5cb2c8444223e05dba42a69ed453a1
Humberto Dias <hldtux@gmail.com>
2024-04-06 15:13:09 -0300

    cp config.guess

--- scripts/007_SDL2_mixer-2.8.0.sh
+++ scripts/007_SDL2_mixer-2.8.0.sh
@@ -4,7 +4,7 @@ wget http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-$VERSION.tar.gz
 ## Unpack the source code.
 rm -Rf SDL2_mixer && mkdir SDL2_mixer && tar --strip-components=1 --directory=SDL2_mixer -xvzf SDL2_mixer.tar.gz && cd SDL2_mixer
 
-cp ../archives/config.sub ../archives/config.guess build-scripts/ || { exit 1; }
+cp ../archives/config.* . || { exit 1; }
 
 #cat ../patches/SDL2_mixer-$VERSION.patch | patch -p1 || { exit 1; }
 
--- scripts/008_SDL2_ttf-2.22.0.sh
+++ scripts/008_SDL2_ttf-2.22.0.sh
@@ -4,6 +4,8 @@ wget https://github.com/libsdl-org/SDL_ttf/releases/download/release-$VERSION/SD
 ## Unpack the source code.
 rm -Rf SDL2_ttf && mkdir SDL2_ttf && tar --strip-components=1 --directory=SDL2_ttf -xvzf SDL2_ttf.tar.gz && cd SDL2_ttf
 
+cp ../archives/config.* . || { exit 1; }
+
 ./configure --prefix="$PS3DEV/portlibs/ppu" --host=powerpc64-ps3-elf \
 	--with-freetype-exec-prefix="$PS3DEV/portlibs/ppu" \
 	--with-sdl-exec-prefix="$PS3DEV/portlibs/ppu" \
